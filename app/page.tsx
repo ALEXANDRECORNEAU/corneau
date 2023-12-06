5a84ddcee7c5b159ec7a4c21ffc12af44d885b3d
ALEXANDRECORNEAU <alexandrecorneau@gmail.com>
2023-12-05 21:28:49 -0300

    ajustes

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,17 @@
-<<<<<<< HEAD
 import Banner from "./components/Banner";
 import Courses from "./components/Courses";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
-=======
-import Admin from "";
-
-
->>>>>>> 8e09919 (.)
 export const revalidate = 0
 export default function Home() {
   return (
     <div className="bg-[#3d3d3d]">
       <div className="max-w-[1440px] m-auto ">
-<<<<<<< HEAD
         <Header />
         <Banner />
         <Courses />
         <Footer />
-=======
-        <Admin/>
-        
->>>>>>> 8e09919 (.)
 
       </div>
     </div>
